6a049ee6ddf4c65438310cc19130de96f2612aa5
Austine Juma <austinejumah001@gmail.com>
2025-07-11 08:35:54 +0300

    Insert interest rate calc in the print func

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,5 @@
 principal = 1000 # Represents $1000
 rate = 0.05 # 5% annual interest rate
 time = 3 # 3 years duration
-interest_rate = principal * rate * time
 
-print("The simple interest is:", interest_rate)
+print("The simple interest is:", principal * rate * time)
