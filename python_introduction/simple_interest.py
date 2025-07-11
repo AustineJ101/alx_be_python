ea17d1c52bf0f46101981dce2885d7ccb4ebd66d
Austine Juma <austinejumah001@gmail.com>
2025-07-11 08:38:02 +0300

    Store the interest rate calc in the interest variable

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,6 @@
 principal = 1000 # Represents $1000
 rate = 0.05 # 5% annual interest rate
 time = 3 # 3 years duration
+interest = principal * rate * time
 
-print("The simple interest is:", principal * rate * time)
+print("The simple interest is:", interest)
