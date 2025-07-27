599ec593f7072385cb5299b8fb4db58c73061f45
Austine Juma <austinejumah001@gmail.com>
2025-07-27 19:16:16 +0300

    include elif check

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,7 +9,7 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 return "Infinity"
-            else:
+            elif:
                 return num1 / num2
 
 
