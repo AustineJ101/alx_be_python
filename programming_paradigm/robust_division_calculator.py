a1066a849430b3dedf054b3f3ff8bde56a62f4e0
Austine Juma <austinejumah001@gmail.com>
2025-08-03 23:42:04 +0300

    Replace print statements with return

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,9 +3,8 @@ def safe_divide(numerator, denominator):
        num = float(numerator)
        denom = float(denominator)
        result = num / denom
-       print(f"The result of the division is {result}")
-       return result
+       return f"The result of the division is {result}"
     except ZeroDivisionError:
-        print("Error: Cannot divide by zero.")
+        return "Error: Cannot divide by zero."
     except ValueError:
-        print("Error: Please enter numeric values only.")
+        return "Error: Please enter numeric values only."
